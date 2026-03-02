7a500627cf8bdcd6a631fba33ac0ac5be106b4fe
muhamed05-IT <muha772005@gmail.com>
2026-03-02 14:31:31 +0000

    Multi-jobs update

--- app/user_manager.py
+++ app/user_manager.py
@@ -7,7 +7,7 @@ class UserManager:
     def __init__(self):
         self._users = {}
 
-    def add_user(self, username, user_data):
+    def add_user(self, username, user_data=None):
         if username in self._users:
             raise ValueError("User already exists")
         self._users[username] = user_data
@@ -17,8 +17,11 @@ class UserManager:
 
     def remove_user(self, username):
         if username not in self._users:
-            raise KeyError("User not found")
+            raise ValueError("User not found")
         return self._users.pop(username)
 
     def list_users(self):
         return dict(self._users)
+
+    def count_users(self):
+        return len(self._users)
